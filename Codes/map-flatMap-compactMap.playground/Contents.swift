4313609dfe21f860685bbe73d46a6a5e1c4c7e2c
Jaehoon-So <jhspacelover@naver.com>
2022-12-23 23:17:01 +0900

    docs: delete

--- Codes/map-flatMap-compactMap.playground/Contents.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-let array = ["1", "2", "3", "4", "five"]
-
-let mapResult = array.map{ Int($0) }
-print(mapResult) // [Optional(1), Optional(2), Optional(3), Optional(4), nil]
-
-let compactMapResult = array.compactMap{ Int($0) }
-print(compactMapResult) // [1, 2, 3, 4]
